ac4bbcc764a8055e8fb83c8d69dd471073a30b50
carlosgr1992 <carlos.gr1992@gmail.com>
2024-02-03 11:27:36 +0100

    Agregado el inputText a la clase Activity2 y lo igualamos al input text del User para guardar el dato correctamente

--- app/src/main/java/com/carlos/practica5_pmdm_cgr/Activity2.kt
+++ app/src/main/java/com/carlos/practica5_pmdm_cgr/Activity2.kt
@@ -2,10 +2,15 @@ package com.carlos.practica5_pmdm_cgr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val userInputText = intent.getStringExtra("userInput")
+        val textViewUserInput: TextView = findViewById(R.id.textViewUserInput)
+        textViewUserInput.text = userInputText
     }
 }
