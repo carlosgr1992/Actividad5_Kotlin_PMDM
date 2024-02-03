398069f443aed969b24cb7ebe26d66d1326c6429
carlosgr1992 <carlos.gr1992@gmail.com>
2024-02-03 01:48:39 +0100

    Creado Activity2 con su lógica para cambiar de Activity al tocar el botón del MainActivity

---
+++ app/src/main/java/com/carlos/practica5_pmdm_cgr/Activity2.kt
@@ -0,0 +1,11 @@
+package com.carlos.practica5_pmdm_cgr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
--- app/src/main/java/com/carlos/practica5_pmdm_cgr/MainActivity.kt
+++ app/src/main/java/com/carlos/practica5_pmdm_cgr/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.carlos.practica5_pmdm_cgr
 
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,12 +19,13 @@ class MainActivity : AppCompatActivity() {
         constraintLayout = findViewById(R.id.constraintLayout)
         btnColor = findViewById(R.id.btnColor)
 
+
+
         btnColor.setOnClickListener {
             // Obtenemos el color actual del fondo
             val currentBackgroundColor = (constraintLayout.background as? ColorDrawable)?.color
             val darkBlueColor = resources.getColor(R.color.dark_blue, theme)
             val lightColor = resources.getColor(R.color.backgroundApp, theme)
-
 
             // Compara y cambia el color del fondo
             if (currentBackgroundColor == darkBlueColor) {
@@ -32,5 +34,12 @@ class MainActivity : AppCompatActivity() {
                 constraintLayout.setBackgroundColor(darkBlueColor)
             }
         }
+
+        val btnActivity2: Button = findViewById(R.id.btnActivity2)
+        btnActivity2.setOnClickListener {
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        }
+
     }
 }
